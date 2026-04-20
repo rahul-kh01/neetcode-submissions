3d647543a906f18cd5b16ba368f09bfc52c9e969
rahul-kh01 <84265501+rahul-kh01@users.noreply.github.com>
2026-04-20 22:58:54 +0530

    Add: buy-and-sell-crypto - submission-5

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-5.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int mnp=INT_MAX,maxprofit=0;
+        for(int c:prices){
+          mnp=min(c,mnp);
+          maxprofit=max(maxprofit,c-mnp);
+        }
+        return maxprofit;
+    }
+};
