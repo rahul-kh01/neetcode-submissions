af2513df69813d37d537f012a219ba21168af44e
rahul-kh01 <84265501+rahul-kh01@users.noreply.github.com>
2026-04-22 10:21:46 +0530

    Add: capacity-to-ship-packages-within-d-days - submission-4

---
+++ Data Structures & Algorithms/capacity-to-ship-packages-within-d-days/submission-4.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+bool canship(vector<int>& nums, int mid,int k){
+    int part=1,sum=0;
+    for(int x:nums){
+        if(sum+x>mid){
+            part++;
+sum=x;
+        }
+        else
+        sum+=x;
+    }
+    return part<=k;
+}
+    int shipWithinDays(vector<int>& nums, int k) {
+        int l=*max_element(nums.begin(),nums.end()),r=accumulate(nums.begin(),nums.end(),0);
+
+        while(l<r){
+            int mid=(l+r)/2;
+            if(canship(nums,mid,k)) r=mid;
+            else
+            l=mid+1;
+        }
+        return l;
+    }
+};
