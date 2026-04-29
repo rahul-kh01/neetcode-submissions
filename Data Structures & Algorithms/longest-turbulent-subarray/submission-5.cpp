0b397e2a649a953d8bd4321ececcb75c8f8f1ff7
rahul-kh01 <84265501+rahul-kh01@users.noreply.github.com>
2026-04-29 09:48:14 +0530

    Add: longest-turbulent-subarray - submission-5

---
+++ Data Structures & Algorithms/longest-turbulent-subarray/submission-5.cpp
@@ -0,0 +1,22 @@
+ class Solution {
+public:
+    int maxTurbulenceSize(vector<int>& nums) {
+        int up=1,down=1,ans=0,prev=nums[0];
+        for(int c:nums){
+            if(c==prev)
+            up=down=1;
+            else if(c<prev){
+                up=down+1;
+                down=1;
+            }
+            else{
+                down=up+1;
+                up=1;
+            }
+            ans=max(ans,max(up,down));
+            prev=c;
+
+        }
+        return ans;
+    }
+};
