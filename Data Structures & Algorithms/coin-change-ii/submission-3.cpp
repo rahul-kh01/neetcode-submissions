47316ce53c96c00e6dac916699c4f4c281c234b1
rahul-kh01 <84265501+rahul-kh01@users.noreply.github.com>
2026-05-02 12:36:18 +0530

    Add: coin-change-ii - submission-3

---
+++ Data Structures & Algorithms/coin-change-ii/submission-3.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        vector<int> dp(amount+1,0);
+        dp[0]=1;
+        for(int c:coins){
+         for(int s=c;s<=amount;s++)
+            dp[s]+=dp[s-c];
+        }
+        return dp[amount];
+    }
+};
