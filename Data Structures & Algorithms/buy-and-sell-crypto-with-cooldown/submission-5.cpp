cc34769c15fef50dc37f621b36355b68f4944787
rahul-kh01 <84265501+rahul-kh01@users.noreply.github.com>
2026-06-10 09:51:00 +0530

    Add: buy-and-sell-crypto-with-cooldown - submission-5

---
+++ Data Structures & Algorithms/buy-and-sell-crypto-with-cooldown/submission-5.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& nums) {
+        int buy=INT_MIN,sell=0,prev2sell=0;
+        for(int x:nums){
+int prevbuy=buy,prevsell=sell;
+buy=max(buy,prev2sell-x);
+sell=max(sell,prevbuy+x);
+prev2sell=prevsell;
+
+        }
+        return sell;
+    }
+};
