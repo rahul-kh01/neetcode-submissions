20febcf10f08a789eac320c7aff63c7b05360bf7
rahul-kh01 <84265501+rahul-kh01@users.noreply.github.com>
2026-06-17 17:57:14 +0530

    Add: boats-to-save-people - submission-19

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-19.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int boats=0,l=0,r=nums.size()-1;
+        while(l<=r){
+            if(nums[l]+nums[r]<=k)
+            l++;
+            r--;
+            boats++;
+        }
+        return boats;
+    }
+};
