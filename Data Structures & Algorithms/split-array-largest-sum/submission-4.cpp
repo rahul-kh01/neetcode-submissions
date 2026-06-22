00828468458eb38f1ebb80a6770c0b8cb29f5e09
rahul-kh01 <84265501+rahul-kh01@users.noreply.github.com>
2026-06-22 09:55:08 +0530

    Add: split-array-largest-sum - submission-4

---
+++ Data Structures & Algorithms/split-array-largest-sum/submission-4.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+int cansplit(vector<int>& nums, int mid, int k){
+    int part=1,sum=0;
+    for(int x:nums){
+        if(sum+x>mid)
+        {
+            sum=x;
+            part++;
+
+        
+        }
+        else
+        sum+=x;
+
+    }
+    return part<=k;
+}
+    int splitArray(vector<int>& nums, int k) {
+       int l=*max_element(nums.begin(),nums.end()),r=accumulate(nums.begin(),nums.end(),0);
+       while(l<r){
+        int mid=(l+r)>>1;
+        if(cansplit(nums,mid,k)) r=mid;
+        else
+        l=mid+1;
+       } 
+       return l;
+    }
+};
