9601543199b600885a09b03a44aedc87e8962ff2
rahul-kh01 <84265501+rahul-kh01@users.noreply.github.com>
2026-06-26 16:10:05 +0530

    Add: is-anagram - submission-12

---
+++ Data Structures & Algorithms/is-anagram/submission-12.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        // sort(s.begin(),s.end());
+        // sort(t.begin(),t.end());
+        int freq[26]={0};
+        if(s.size()!=t.size()) return false;
+        for(int i=0;i<s.size();i++)
+        freq[s[i]-'a']++,freq[t[i]-'a']--;
+
+       for(int x:freq)
+       if(x) return false;
+       return true;
+    }
+};
